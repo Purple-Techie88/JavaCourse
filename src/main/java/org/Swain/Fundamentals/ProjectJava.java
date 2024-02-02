d308fe0b2f6f5f87e554533bb83b394f768538ff
LaMorica Swain <lamoricaswain@LaMoricas-Air.attlocal.net>
2024-02-01 22:20:02 -0500

    first commit

---
+++ src/main/java/org/Swain/Fundamentals/ProjectJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.Swain.Fundamentals;
+
+/**
+ *
+ * @author lamoricaswain
+ */
+public class ProjectJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/org/Swain/chapter2/MadLibs.java
@@ -0,0 +1,36 @@
+package org.Swain.chapter2;
+
+import java.util.Scanner;
+
+/*Create a program that asks a user for a season of the year, 
+then a whole number, then an adjective. Use the input to 
+complete the senence below then output the result:
+
+"On a [adjective][season of the year] day, I drink a 
+minimum of [whole number] cups of cofee"
+*/
+public class MadLibs {
+
+    public static void main (String arg[]){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a season of the year:");
+        String season = scanner.next();
+
+        System.out.println("Enter an adjective:");
+        String adjective = scanner.next();
+        
+
+        System.out.println("Enter a whole number:");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("On a "+ adjective + " " + season + " day, I drink a \n" + //
+                "minimum of "+ number+ " cups of coffee.");
+
+
+
+
+    }
+}
