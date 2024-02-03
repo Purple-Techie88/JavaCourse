4cd57d1acd346622b09bec0e8c815d470de69e89
LaMorica Swain <lamoricaswain@LaMoricas-Air.attlocal.net>
2024-02-03 17:30:04 -0500

    clean up

--- src/main/java/org/Swain/chapter2/GrossPayCalculator.java
+++ src/main/java/org/Swain/chapter2/GrossPayCalculator.java
@@ -3,7 +3,8 @@ package org.Swain.chapter2;
 import java.util.Scanner;
 
 public class GrossPayCalculator {
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         //1. Get the number of hours worked 
         System.out.println("Enter the number of hours the employee worked.");
         Scanner scanner = new Scanner(System.in);
