0eb5b4517cffce2289bc172bd52c9cda4db28701
LaMorica Swain <lamoricaswain@LaMoricas-Air.attlocal.net>
2024-02-05 23:57:32 -0500

    add changes

---
+++ src/main/java/org/Swain/chapter3/GradeMessage.java
@@ -0,0 +1,23 @@
+package org.Swain.chapter3;
+
+import java.util.Scanner;
+
+public class GradeMessage {
+    public static void main(String aTrgs[]){
+        System.out.println("Enter your letter grade:");
+        Scanner scanner = new Scanner(System.in);
+        String grade =  scanner.next();
+        scanner.close();
+
+        String message = switch (grade) {
+            case "A" -> "Excellent Job!";
+            case "B" -> "Great Job!";
+            case "C" -> "Good Job!";
+            case "D" -> "You need to work a bit harder!";
+            case "F" -> "Uh oh!";
+            default -> "Error. Invalid grade.";
+        };
+
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/org/Swain/chapter3/LogicalOperatorLoanQualifier.java
@@ -0,0 +1,34 @@
+package org.Swain.chapter3;
+
+import java.util.Scanner;
+/*
+ * Logical operators:
+ * To qualify for a loan, a person must make at least $30,000
+ * and have been woring at their current job for at least 2 years.
+ */
+
+public class LogicalOperatorLoanQualifier {
+    public static void main(String[] args) {
+         //Initialize what we know 
+        int requiredSalary = 30000;
+        int requiredYearsEmployed = 2;
+
+        //Get what we don't 
+        System.out.println("Enter your salary:");
+        Scanner scanner = new Scanner(System.in);
+        double salary = scanner.nextDouble();
+
+        System.out.println("Enter the number of years with your current employer:");
+        double years = scanner.nextDouble();
+        scanner.close();
+        //Make decision
+        if (salary >= requiredSalary && years >= requiredYearsEmployed) {
+                System.out.println("Congrats! You qualify for the loan!");            
+        } 
+        else {
+            System.out.println("Sorry, you must earn at least $" + requiredSalary
+                    + " and have worked at least " +requiredYearsEmployed + " years to qualify for the loan.");
+        }
+    }
+        
+}
